f663a27405eb51958ceb715dc06849fa7943ce9b
rezesidzerezi <rezi74170@gmail.com>
2025-07-18 01:06:05 +0400

    commit

---
+++ day02/homework/hw1.py
@@ -0,0 +1,126 @@
+#part1
+
+
+num1 = 100
+num2 = 50
+
+num1 + num2 
+
+num1 - num2 
+
+num1 * num2
+
+num1 / num2
+
+
+#part2
+
+#string type data
+name = 'rezi'
+country = "saqartvelo"
+gear = "phone"
+car = "BMW"
+#integer type data
+age = 14
+num1 = 100
+num2 = 200
+num3 = 1000
+#float type data
+height = 1.70
+weight = 50.25
+decimal1 = 100.50
+decimal2 = 20.67
+print(name, country, gear, car, age, num1, num2, num3, height, weight, decimal1, decimal2)
+
+#part3
+name = "rezi"
+car = "BMW"
+print(name + car)
+#სტრინგების დამატებას ეწოდება კონკატინაცია
+
+#part4
+name = "rezi"
+name2 = "goga"
+name3 = "dachi"
+name4 = "gega"
+name5 = "luka"
+print(name + name2 + name3 + name4 + name5)
+
+#part5
+age = 14
+height = 1.70
+num1 = age * height 
+num2 = age + height 
+num3 = age - height
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
