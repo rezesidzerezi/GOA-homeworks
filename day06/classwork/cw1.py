4f8a65fcb0e83aa77dc303ca88a49b3c35282245
rezesidzerezi <rezi74170@gmail.com>
2025-07-20 15:14:09 +0400

    commit

---
+++ day06/classwork/cw1.py
@@ -0,0 +1,21 @@
+#part1
+#logical operatons and,or 
+#and  თუ ლოგიკურ ოპერაციაში არის ერთი მაინც false მაშინ იქნება false მაგ:თუ არის folse da true პასუხი იქნება false
+#or  თუ ლოგიკურ ოპერაციაში არის ერთი მაინც true მაშინ პასუხი იქნება true მაგ:false da true პასუხი იქნება true
+
+#part2
+name = input("whats your name")
+name1 = "rezi"
+print(name == name1)
+
+
+#part3
+print(True and False)
+print(True or False)
+
+
+
+
+
+
+
