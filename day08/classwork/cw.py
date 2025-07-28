7dabd4a936f032537b0e3ec6395337de3831ec8f
rezesidzerezi <rezi74170@gmail.com>
2025-07-28 19:03:47 +0400

    commit

---
+++ day08/classwork/cw.py
@@ -0,0 +1,32 @@
+print(55==55 and 77>55)
+print(100>50 and 55<145)
+print(88==88 and 77>66)
+
+print(1000==44 or 66>233)
+print(454<123 or 87==55)
+print(789==666 or 78>78)
+
+
+for i in range(10):
+     print("rezi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
