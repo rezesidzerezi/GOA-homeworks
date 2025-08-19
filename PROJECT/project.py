b46aaf68d3dd4eb08f607296029f5964c70aec98
rezesidzerezi <rezi74170@gmail.com>
2025-08-19 18:47:12 +0400

    commit

--- PROJECT/project.py
+++ PROJECT/project.py
@@ -1,13 +1,13 @@
-import time
-import sys
+import time #time
+import sys #System-specific parameters and functions
 
 
 def loading_screen_2(choice_option):
  for i in range(3):  # how many times it repeats
-    for dots in range(4):  # "", ".", "..", "..."
+    for dots in range(4):  
         sys.stdout.write("\rLoading transaction history" + "." * dots + "   ")  # overwrite line
         
-        time.sleep(0.5)  # delayc
+        time.sleep(0.5)  # delay
         if i == 2 and dots == 3:
             if choice_option == "2":
                 print("\r ")
@@ -16,12 +16,9 @@ def loading_screen_2(choice_option):
 
          
 
-
-
-
 def loading_screen_1(choice_option):
  for i in range(2):  # how many times it repeats
-    for dots in range(4):  # "", ".", "..", "..."
+    for dots in range(4):  
         sys.stdout.write("\rLoading " + "." * dots + "   ")  # overwrite line
         
         time.sleep(0.5)  # delayc
@@ -55,9 +52,13 @@ def loading_screen_1(choice_option):
         
 
         
+
+
+
+
 def loading_screen_0(choice_option):
  for i in range(2):  # how many times it repeats
-    for dots in range(4):  # "", ".", "..", "..."
+    for dots in range(4):  
         sys.stdout.write("\rLoading" + "." * dots + "   ")  # overwrite line
         
         time.sleep(0.5)  # delayc
@@ -66,6 +67,8 @@ def loading_screen_0(choice_option):
 
          if choice_option == "1":
            print("\rlogin successful,")
+
+
 
 
 
@@ -85,15 +88,17 @@ exchange_rates = {
     "EUR": {"GEL": 2.95, "USD": 1.09}
 }
 
-
 accounts = {
-    "saba": {"password": "1111", "balance": 30.0, "currency": "GEL"},
-    "gia": {"password": "2222", "balance": 20.0, "currency": "USD"},
-    "admin": {"password": "admin", "balance": 0.0, "currency": "GEL"}
+    "saba": {"password": "1111", "balance": 30.0, "history": [], "currency": "GEL" },
+    "gia": {"password": "2222", "balance": 20.0, "history": [], "currency": "USD"  },
+    "admin": {"password": "admin", "balance": 0.0, "history": [], "currency": "GEL"} 
+
 }
 
+
+
 transaction_fee = 0.10
-history_log = []
+
 
 
 def convert(amount, from_currency, to_currency):
@@ -101,7 +106,7 @@ def convert(amount, from_currency, to_currency):
         return amount
     return amount * exchange_rates[from_currency][to_currency]
 
-slow_print(" Welcome to group 2 Bank ")
+slow_print(" Welcome to goavalut")
 
 
 while True:
@@ -147,7 +152,7 @@ while True:
                             converted = convert(amount, dep_currency, accounts[username]['currency'])
                             accounts[username]['balance'] += (converted - transaction_fee)
                             slow_print(f" {amount} {dep_currency} converted to {converted:.2f} {accounts[username]['currency']}. Fee applied.")
-                            history_log.append(f"{username} deposited {amount}{dep_currency} -> {converted:.2f}{accounts[username]['currency']}, fee {transaction_fee}{accounts[username]['currency']}")
+                            accounts[username]["history"].append(f"{username} deposited {amount}{dep_currency} -> {converted:.2f}{accounts[username]['currency']}, fee {transaction_fee}{accounts[username]['currency']}")
                         else:
                             slow_print("Amount too small (must be larger than the fee).")
 
@@ -156,10 +161,10 @@ while True:
                         amount = float(slow_input("Enter withdrawal amount: "))
                         total = amount + transaction_fee
                         if total <= accounts[username]['balance']:
-                            accounts[username]['balance'] -= total
+                            accounts[username]['balance'] -=  total
                             loading_screen_1(bank_choice)
                             slow_print(f"Withdrawal successful! You took {amount} {accounts[username]['currency']}. Fee applied.")
-                            history_log.append(f"{username} withdrew {amount}{accounts[username]['currency']}, fee {transaction_fee}{accounts[username]['currency']}")
+                            accounts[username]["history"].append(f"{username} withdrew {amount}{accounts[username]['currency']}, fee {transaction_fee}{accounts[username]['currency']}")
                         else:
                             slow_print("Insufficient funds!")
 
@@ -177,7 +182,9 @@ while True:
                                 accounts[recipient]['balance'] += converted_amount
                                 loading_screen_1(bank_choice)
                                 slow_print(f"Transfer successful! {amount} {accounts[username]['currency']} converted to {converted_amount:.2f} {accounts[recipient]['currency']}.")
-                                history_log.append(f"{username} transferred {amount}{accounts[username]['currency']} -> {recipient} ({converted_amount:.2f}{accounts[recipient]['currency']}), fee {transaction_fee}{accounts[username]['currency']}")
+                                accounts[username]["history"].append(f"{username} transferred {amount}{accounts[username]['currency']} -> {recipient} ({converted_amount:.2f}{accounts[recipient]['currency']}), fee {transaction_fee}{accounts[username]['currency']}")
+                                accounts[recipient]['history'].append(f"{username} transferred {amount}{accounts[username]['currency']} -> {recipient} ({converted_amount:.2f}{accounts[recipient]['currency']}), fee {transaction_fee}{accounts[username]['currency']}")
+                                
                             else:
                                 slow_print("Insufficient funds!")
                         else:
@@ -197,33 +204,24 @@ while True:
                                 new_pass = slow_input("Enter new password: ")
                                 accounts[username]["password"] = new_pass
                                 slow_print("Password changed successfully.")
-                                history_log.append(f"{username} changed password")
+                                accounts[username]["history"].append(f"{username} changed password")
 
 
                             elif settings_option == "2":
                                 loading_screen_2(settings_option)
                                 slow_print("Transaction History:")
-                                if username == "admin":
-                                    if not history_log:
-                                        slow_print("No transactions yet.")
-                                    else:
-                                        for entry in history_log:
-                                            slow_print(" - " + entry)
+                                if not accounts[username]["history"]:
+                                    slow_print("No transactions yet.")
                                 else:
-                                    found = False
-                                    for entry in history_log:
-                                        if entry.startswith(username):
-                                            slow_print("- " + entry)
-                                            found = True
-                                    if not found:
-                                        slow_print("No transactions found for your account.")       
+                                    for entry in accounts[username]["history"]:
+                                     slow_print("- " + entry)    
 
                             elif settings_option == "3":
                                 confirm = slow_input("Are you sure you want to delete your account? (yes/no): ")
                                 if confirm.lower() == "yes":
                                     del accounts[username]
                                     slow_print("Your account has been deleted.")
-                                    history_log.append(f"{username} deleted their account")
+                                    accounts[username]["history"].append(f"{username} deleted their account")
                                     break
 
                             elif settings_option == "4":
@@ -259,7 +257,7 @@ while True:
             if new_currency not in exchange_rates:
                 slow_print("Unsupported currency, defaulting to GEL.")
                 new_currency = "GEL"
-            accounts[new_user] = {"password": new_password, "balance": 0.0, "currency": new_currency}
+            accounts[new_user] = {"password": new_password, "balance": 0.0, "history": [], "currency": new_currency}
             slow_print(f"Account created successfully with currency {new_currency}.")
 
     else:
