5e587a9a5905c55598218133f362a3ecb2bb7d4b
rezesidzerezi <rezi74170@gmail.com>
2026-02-22 20:05:14 +0400

    commit

---
+++ level 38/classwork/cw.py
@@ -0,0 +1,71 @@
+# codewars1
+
+
+# def multiply(a, b):
+#     return a * b
+
+
+
+# codewars2
+
+# def even_or_odd(number):
+#     if number % 2 == 0:
+#         return "Even"
+#     elif number % 2 != 0:
+#         return "Odd"
+
+
+
+
+
+# codewars3
+
+# def number_to_string(num):
+#     return str(num)
+
+
+
+
+
+# codewars4
+
+# def solution(string):
+#     return string[::-1]
+
+
+
+
+# codewars5
+
+# def make_negative( number ):
+#     if number >= 0:
+#         return -number
+#     else:
+#         return number
+
+
+
+# codewars6
+
+# def bool_to_word(boolean):
+#     if boolean == True:
+#         return "Yes"
+#     else:
+#         return "No"
+
+
+
+#codewars7
+
+# def find_smallest_int(arr):
+#     smallest = arr[0]
+#     for i in arr:
+#         if i < smallest:
+#             smallest = i
+#     return smallest
+
+
+
+
+
+
---
+++ level 38/homework/hw.py
@@ -0,0 +1,43 @@
+# codewars1
+
+# def repeat_str(repeat, string):
+#     return string * repeat
+
+
+
+# codewars2
+
+# def get_count(sentence):
+#     count = 0
+#     vovel = "aeiou"
+#     for i in sentence:
+#         if i in vovel:
+#             count += 1
+#     return count
+
+
+
+# codewars3
+
+# def positive_sum(arr):
+#     sum = 0
+#     for i in arr:
+#         if i >= 0:
+#             sum += i
+#     return sum
+
+
+
+# codewars4
+
+# def string_to_number(s):
+#     return int(s)
+
+
+
+# codewars5
+
+# def greet():
+#     return "hello world!"
+
+
