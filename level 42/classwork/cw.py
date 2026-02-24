ed460f290670f5f4138561c0c51eb8101588550d
rezesidzerezi <rezi74170@gmail.com>
2026-02-24 23:38:20 +0400

    commit

---
+++ level 42/classwork/cw.py
@@ -0,0 +1,54 @@
+# codewars 1
+
+# def no_space(x):
+#     string = ""
+#     for i in x:
+#         if i != " ":
+#             string += i
+#     return string
+
+
+
+# codewars 2
+
+# def count_sheeps(sheep):
+#     count = 0
+#     for i in sheep:
+#         if i == True:
+#             count += 1
+#     return count
+
+
+# codewars 3
+
+# def summation(num):
+#     sum = 0
+#     for i in range(1,num + 1):
+#         sum += i
+#     return sum
+
+# codewars 4
+
+# def twice_as_old(dad_years_old , son_years_old):
+#     age = dad_years_old - son_years_old*2
+#     if age < 0:
+#         return -age
+#     else:
+#         return age
+
+# codewars 5
+
+# def number(bus_stops):
+#     get_on = 0
+#     get_off = 0
+#     for i in bus_stops:
+#         get_on += i[0]
+#         get_off += i[1]
+#     return get_on - get_off
+
+
+
+
+
+
+
---
+++ level 42/homework/hw.py
@@ -0,0 +1,49 @@
+# codewars 1
+
+# def remove_char(s):
+#     return s[1:-1]
+
+
+# codewars 2
+
+# def double_integer(i):
+#     return i*2
+
+
+# codewars 3
+
+# def greet(name):
+#     return "Hello, " + name + " how are you doing today?"
+
+
+# codewars 4
+
+# def basic_op(operator, value1, value2):
+#     if operator == '+':
+#         return value1 + value2
+#     elif operator == '-':
+#         return value1 - value2
+#     elif operator == '*':
+#         return value1 * value2
+#     elif operator == '/':
+#         return value1 / value2
+
+
+# codewars 5
+
+# def sum_array(a):
+#     sum = 0
+#     for i in a:
+#         sum += i
+#     return sum
+
+
+# codewars 6
+
+# def simple_multiplication(number) :
+#     if number % 2 == 0:
+#         return number * 8
+#     else:
+#         return number * 9
+
+
