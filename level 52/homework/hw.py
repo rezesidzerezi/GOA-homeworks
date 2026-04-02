f1c395cf7bb5107df07ea59c3ba2b04a86b44ca2
rezesidzerezi <rezi74170@gmail.com>
2026-04-02 21:46:44 +0400

    commit

--- level 52/homework/hw.py
+++ level 52/homework/hw.py
@@ -50,7 +50,23 @@
 
 # codewars 5
 
+# def remove(s):
+#     new = ""
+#     count = 0
+    
+#     i = len(s) - 1
+#     while i > 0 and s[i] == "!":
+#         count += 1
+#         i -= 1
+    
+#     for i in s:
+#         if i != "!":
+#             new += i
+    
+#     for i in range(count):
+#         new += "!"
 
+#     return new
 
 
 # codewars 6
