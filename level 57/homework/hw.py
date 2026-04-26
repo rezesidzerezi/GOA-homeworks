6dbe7e49b8e56a017c1ac5f16fec3837405ad58d
rezesidzerezi <rezi74170@gmail.com>
2026-04-26 23:55:46 +0400

    commit

--- level 57/homework/hw.py
+++ level 57/homework/hw.py
@@ -45,4 +45,50 @@
 
 # codewars 5
 
+# def adjacent_element_product(array):
+#     max_product = array[0] * array[1]
+
+#     i = 1
+#     while i < len(array) - 1:
+#         product = array[i] * array[i + 1]
+#         if product > max_product:
+#             max_product = product
+#         i += 1
+#     return max_product
+
+# codewars 6
+
+# def show_sequence(n):
+#     if n<0:
+#         return str(n)+ "<0"
+#     elif n == 0:
+#         return "0=0"
+#     st=''
+#     jami=0
+#     for i in range(n+1):
+#         st+=f"{(i)}+"
+#         jami+=i
+
+#     return st[0 :- 1]+' = '+str(jami)
+
+# codewars 7
+
+# def solution(digits):
+#     max_num = 0
+
+#     for i in range(len(digits) - 4):
+#         num = int(digits[i:i+5])
+#         if num > max_num:
+#             max_num = num
+
+#     return max_num
+
+# codewars 8
+
+# def factorial(n):
+#     sum = 1
+#     for i in range(1,n+1):
+#         sum = sum * i
+#     return sum
+
 
---
+++ level 60/homework/hw.py
@@ -0,0 +1,33 @@
+# codewars 1
+
+# def is_valid_walk(walk):
+#     if len(walk) == 10 and walk.count("n") == walk.count("s") and walk.count("w") == walk.count("e"):
+#         return True
+#     else:
+#         return False
+
+# codewars 2
+
+# def compute_sum(n):
+#     sum1 = 0
+#     for i in range(1, n + 1):
+#         for ii in str(i):
+#             sum1 += int(ii)
+#     return sum1
+
+# codewars 3
+
+# def delete_nth(order,max_e):
+#     filltered = []
+#     for i in order:
+#         if filltered.count(i) < max_e:
+#             filltered.append(i)
+#     return filltered
+
+# codewars 4
+
+
+
+# codewars 5
+
+
