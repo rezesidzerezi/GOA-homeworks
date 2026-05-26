d4206eb7b0b271a5a7a8803f2ad76a05bdbb05bd
rezesidzerezi <rezi74170@gmail.com>
2026-05-26 23:21:26 +0400

    1

---
+++ level 69/classwork/cw.py
@@ -0,0 +1,59 @@
+# codewars 1
+
+# def sort_gift_code(code):
+#     return "".join(sorted(code))
+
+# codewars 2
+
+# def square_digits(num):
+#     num1 = ""
+
+#     for i in str(num):
+#         num1 += str(int(i) * int(i))
+
+#     return int(num1)
+
+# codewars 3
+
+# def multiples(m: int, n: int | float) -> list[int] | list[float]:
+#     new = []
+#     for i in range(1,m+1):
+#         new.append(n * i)
+#     return new
+
+# codewars 4
+
+# def divisors(n):
+#     countnum = 0
+
+#     for i in range(1, n+1):
+#         if n % i == 0:
+#             countnum += 1
+#     return countnum
+
+# codewars 5
+
+# def dont_give_me_five(start,end):
+#     final = 0
+#     for i in range(start , end + 1):
+#         if '5' not in str(i):
+#             final += 1
+#     return final
+
+# codewars 6
+
+# def longest(a1, a2):
+#     a = a1 + a2
+#     b = ''
+#     for i in a:
+#         if i not in b:
+#             b += i
+#     return ''.join(sorted(b))
+
+# codewars 7
+
+# def sort_by_length(arr):
+#     return sorted(arr , key = len)
+
+
+
