4dc214fd2ecd6e61373e4d206220c0f4e929a45a
rezesidzerezi <rezi74170@gmail.com>
2026-05-28 21:04:52 +0400

    Update hw.py

---
+++ level 69/homework/hw.py
@@ -0,0 +1,19 @@
+# codewars 1
+
+# def spacey(array):
+#     new = []
+#     string = ""
+#     for i in array:
+#         string += i
+#         new.append(string)
+#     return new
+
+# codewars 2
+
+
+
+
+
+
+
+
