611e59391481408fbd91fc1024e156e36a776b1c
rezesidzerezi <rezi74170@gmail.com>
2026-05-31 16:53:04 +0400

    Update hw.py

--- level 69/homework/hw.py
+++ level 69/homework/hw.py
@@ -10,7 +10,20 @@
 
 # codewars 2
 
+# def cube_odd(arr):
+#     result = 0
+#     for i in arr:
+#         if type(i) != int:
+#             return None
+        
+#         new_i = i ** 3
+        
+#         if new_i % 2 != 0:
+#             result += new_i
+            
+#     return result
 
+# codewars 3
 
 
 
