7f93d014b6f3de0db83449594ca02a5f6e85f89d
rezesidzerezi <rezi74170@gmail.com>
2026-06-23 20:42:23 +0400

    1

---
+++ level 75/homework/hw.py
@@ -0,0 +1,25 @@
+
+# codewars 2
+
+# def to_binary(n):
+#     result = ""
+
+#     for i in range(n):
+#         if n == 0:
+#             break
+
+#         result = str(n % 2) + result
+#         n = n // 2
+
+#     return int(result)
+
+
+
+
+
+
+
+
+
+
+
