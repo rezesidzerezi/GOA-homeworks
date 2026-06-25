627f4da4c7e68f2ae65eff665391c02c611e0195
rezesidzerezi <rezi74170@gmail.com>
2026-06-25 19:04:16 +0400

    1

---
+++ level 73/homework/hw.py
@@ -0,0 +1,27 @@
+# codewars 1
+
+# def mxdiflg(a1, a2):
+#     l = []
+#     if a1 == [] or a2 == []:
+#         return -1
+    
+#     for i in a1:
+#         for b in a2:
+#             l.append(abs(len(i) - len(b)))
+    
+#     return max(l)
+
+# codewars 4
+
+# def find_longest(arr):
+#     string = []
+#     for i in arr:
+#         string.append(str(i))
+    
+#     max = string[0]
+#     for i in string:
+#         if len(i) > len(max):
+#             max = i
+#     return int(max)
+
+
