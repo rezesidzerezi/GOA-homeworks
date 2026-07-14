bd5d243e54753ad441b2f2c1e4315613ad49878a
rezesidzerezi <rezi74170@gmail.com>
2026-07-14 22:53:41 +0400

    1

---
+++ level 80/classwork/cw.py
@@ -0,0 +1,66 @@
+# codewars 1
+
+# def calc_type(a, b, res) -> str:
+#     if a + b == res:
+#         return "addition"
+#     elif a - b == res:
+#         return "subtraction"
+#     elif a * b == res:
+#         return "multiplication"
+#     else:
+#         return "division"
+
+# codewars 2
+
+# def break_chocolate(n, m):
+#     if n * m == 1 or n * m == 0:
+#         return 0
+#     return n * m - 1
+
+# codewars 3
+
+# def hero(bullets, dragons):
+#     if bullets >= dragons * 2:
+#         return True
+#     else:
+#         return False
+
+# codewars 4
+
+# def find_needle(haystack):
+#     for i in range(len(haystack)):
+#         if haystack[i] == "needle":
+#             return "found the needle at position " + str(i)
+
+# codewars 5
+
+# def unusual_five():
+#     return len("rezii")
+
+# codewars 6
+
+# def make_negative( number ):
+#     if number >= 0:
+#         return -number
+#     else:
+#         return number
+
+# codewars 7
+
+# def double_integer(i):
+#     return i*2
+
+# codewars 8
+
+# def accum(st):
+#     word = ""
+    
+#     for i in range(len(st)):
+#         word += st[i].upper() + st[i].lower() * i
+#         if i != len(st) - 1:
+#             word += "-"
+#     return word
+
+# codewars 9
+
+
