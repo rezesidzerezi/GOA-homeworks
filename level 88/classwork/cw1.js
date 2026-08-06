23b13a20fdaf0d1545ee81b44dcbec0442888379
rezesidzerezi <rezi74170@gmail.com>
2026-08-06 14:24:57 +0400

    1

---
+++ level 88/classwork/cw1.js
@@ -0,0 +1,24 @@
+console.log(10 + 67)
+console.log(10 - 67)
+console.log(10 * 67)
+console.log(10 / 67)
+console.log(10 % 67)
+
+console.log("hello" + " my" + " name" + " is" + " Rezi")
+
+
+console.log("10" - 67)
+console.log("10" * 67)
+console.log("10" / 67)
+console.log("10" % 67)
+
+console.log("rezi".length)
+console.log("rezi".toUpperCase())
+console.log("rezi".toLowerCase())
+console.log("rezi".startsWith("g"))
+console.log("          rezi              ".trim())
+console.log("ჰიდროელექტროსადგური".slice(5,12))
+
+
+
+
