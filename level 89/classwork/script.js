9962604c574d6fca28503113a759a9cfa97eb9f0
rezesidzerezi <rezi74170@gmail.com>
2026-08-07 13:15:55 +0400

    1

---
+++ level 89/classwork/script.js
@@ -0,0 +1,38 @@
+//1)შენი დავალებაა რომ დაბეჭდო შენი სახელი 15 ჯერ
+
+console.log("rezi".repeat(15))
+
+/*2)შენი დავალებაა რომ სტრინგში --> "i am going to dubai" სიტყვა dubai ჩაანაცვლო სიტყვა "Tbilisi" ით*/
+
+console.log("i am going to dubai".replace("dubai", "Tbilisi"))
+
+/*3)შენი დავალებაა რომ სრრინგში -->"my friend told me to tell my other friend to come" შენი დავალება ყველა  friend ჩაანაცვლო სიტყვა --> bro ით*/ 
+
+console.log("my friend told me to tell my other friend to come".replaceAll("friend",
+"bro"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
