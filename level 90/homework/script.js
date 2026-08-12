40b168c07ac9438862b322f520999911857e38bb
rezesidzerezi <rezi74170@gmail.com>
2026-08-12 17:19:01 +0400

    1

---
+++ level 90/homework/script.js
@@ -0,0 +1,17 @@
+/*1)შექმენი 4 ცვლადი let ის გამოყენებით,
+
+1)name
+2)surname
+3)age
+4)adress
+
+გამოიტანე შემდეგნაირი ტექსტი : hello,my name is ... ,my surname is ... and my age is ... age , i live in ... . 
+
+სამი წერტილის მაგივრად უნდა ჩასვათ ცვლადის მნიშვნელობები,გამოიყენეთ string interpolation --> `...${}...`*/
+
+
+
+
+
+
+
