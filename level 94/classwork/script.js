bf1e854bf5bc1f75339b823d3c0010f7581177f2
rezesidzerezi <rezi74170@gmail.com>
2026-08-27 13:51:37 +0400

    1

--- level 94/classwork/script.js
+++ level 94/classwork/script.js
@@ -16,3 +16,27 @@ let checkNumber = function(number){
     :number < 0 && number % 2 != 0 ? console.log("Negative Odd")
     : console.log("Zero")
 }
+
+/*2)შექმენით arrow ფუნქცია რომელსაც გადაეცემა ერთ პარამეტრი name ფუნქციამ
+უნდა შეამოწმოს if else ით იწყება თუ არა ეს სახელი "გ" ასოზე , თუ იწყება
+დააბრუნე good name სხვა შემთხვევაში დააბრუნე "still good name"*/
+
+let idk = name => {
+    if(name.startsWith("g")){
+        console.log("good name")
+    }else{
+        console.log("still good name")
+    }
+}
+
+idk("rezi")
+
+
+/*3)შექმენით arrow ფუნქცია რომელსაც გადაეცემა ერთ პარამეტრი num ,
+შენი დავალებაა შეამოწმო ეს რიცხვი ლუწია თუ კენტია ,
+use ternary and single line block stytax*/ 
+
+let idk2 = num => num % 2 == 0 ? "Even" : "Odd"
+
+idk2(18)
+
